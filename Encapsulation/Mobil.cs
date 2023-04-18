380d1c0b349d5e11b29e63e92a6af01dbe007017
Cornelia <cornelia@students.amikom.ac.id>
2023-04-18 18:57:21 +0700

    first commit

---
+++ Encapsulation/Mobil.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    internal class Mobil
+    {
+        private string merk;
+        private string warna;
+        private int tahunkeluaran;
+        
+
+        public Mobil(string merk, string warna, int tahunkeluaran)
+        {
+            this.merk = merk;
+            this.warna = warna;
+            this.tahunkeluaran = tahunkeluaran;  
+        }
+        public string Merk
+        {
+            get { return merk; }
+            set { merk = value; }
+        }
+
+        public string Warna
+        {
+            get { return warna; }
+            set { warna = value; }
+        }
+
+        public int Tahunkeluaran
+        {
+            get { return tahunkeluaran; }
+            set { tahunkeluaran = value; }
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine("Merk: " + merk);
+            Console.WriteLine("Warna: " + warna);
+            Console.WriteLine("Tahunkeluaran: " + tahunkeluaran);
+        }
+    }
+}
---
+++ Encapsulation/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mobil mobil1 = new Mobil("Toyota", "Hitam", 2022);
+
+
+            mobil1.Merk = "Toyota";
+            mobil1.Warna = "Hitam";
+            mobil1.Tahunkeluaran = 2022;
+            mobil1.DisplayInfo();
+ 
+        }
+    }
+}
+
+            
+
